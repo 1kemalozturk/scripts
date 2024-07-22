1f192b012c3b0a43e7e1f0c2ad82b8a4947f1281
Kemal OZTURK <32194426+1kemalozturk@users.noreply.github.com>
2024-07-22 17:13:33 +0300

    Update oneclick.sh

--- linux/oneclick.sh
+++ linux/oneclick.sh
@@ -649,7 +649,7 @@ homeassistant_install_supervised() {
         fi
 
         # getumbrel Services
-        if [ -n "$(docker ps --format json | jq -r .Image | grep -E 'getumbrel/auth-server|getumbrel/tor')" ]; then
+        if [ -n "$(docker ps --format json | jq -r .Names | grep -E 'auth|tor_proxy')" ]; then
             if [ -n "$(docker ps --format json | jq -r .Names | grep -E 'gifted_almeida')" ]; then
             echo "Container gifted_almeida is already running."
             else
